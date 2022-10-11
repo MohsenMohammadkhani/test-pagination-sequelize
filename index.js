53d6bbf4801b3430b9e5ce59de63e2d28d8767f3
Mohsen Mohammadkhani <mohsenmohammadkhanigla@gmail.com>
2022-10-11 09:48:15 +0330

    fix bug in pagination

--- index.js
+++ index.js
@@ -6,50 +6,52 @@ const app = express();
 const port = 4000;
 
 app.get("/data", async (req, res) => {
-  const page = parseInt(req.query.page);
-  const number = parseInt(req.query.number);
+  const currentPage = parseInt(req.query.page);
+  const countItemsPerPage = parseInt(req.query.number);
   const countItems = await getCountItem();
-  const offset = getOffsetPagination(countItems, number, page);
-  let limit = number;
-  if (!offset) {
-    limit = number * page - countItems;
-    if (!limit) {
-      limit = number;
-    }
-  }
-  const optionsForFindAndCountAll = {
-    limit,
-    offset,
-  };
 
+  const offset = getOffsetPagination(
+    countItems,
+    countItemsPerPage,
+    currentPage
+  );
+  const countPages = getCountPages(countItems, countItemsPerPage);
+  const limit = getLimit(
+    countItemsPerPage,
+    currentPage,
+    countItems,
+    countPages
+  );
+
+  let where = {};
   if (req.query.email) {
-    optionsForFindAndCountAll["where"] = {
+    where["where"] = {
       email: {
         [Op.like]: `${req.query.email}%`,
       },
     };
   }
 
-  const rows = await Model.findAll(optionsForFindAndCountAll);
+  const rows = await Model.findAll({ ...where, offset, limit });
 
   let data = rows.map((position) => {
     return position.dataValues;
   });
-  data=data.reverse()
+  data = data.reverse();
 
   const response = {
     count_items: countItems,
-    count_page: Math.ceil(countItems / number),
+    count_page: countPages,
     data,
   };
-  if (page > 1) {
+  if (currentPage > 1) {
     response["prevPage"] = `http://localhost:4000/data?page=${
-      page - 1
-    }&number=${number}`;
+      currentPage - 1
+    }&number=${countItemsPerPage}`;
   }
   response["nextPage"] = `http://localhost:4000/data?page=${
-    page + 1
-  }&number=${number}`;
+    currentPage + 1
+  }&number=${countItemsPerPage}`;
 
   res.send(response);
 });
@@ -58,12 +60,23 @@ const getCountItem = async () => {
   return await Model.count();
 };
 
-const getOffsetPagination = (countItems, countPerPage, currentPage) => {
-  let offset = countItems - countPerPage * currentPage;
+const getCountPages = (countItems, countItemsPerPage) => {
+  return Math.ceil(countItems / countItemsPerPage);
+};
+
+const getOffsetPagination = (countItems, countItemsPerPage, currentPage) => {
+  let offset = countItems - countItemsPerPage * currentPage;
   if (offset < 0) {
     return 0;
   }
   return offset;
+};
+
+const getLimit = (countItemsPerPage, currentPage, countItems, countPages) => {
+  if (currentPage == countPages) {
+    return countItems - countItemsPerPage * (currentPage - 1);
+  }
+  return countItemsPerPage;
 };
 
 app.listen(port);
